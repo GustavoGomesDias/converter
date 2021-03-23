267cad159f4921b8091a16f00bb72ce2df8052b5
GustavoGomesDias <treeelevengg@gmail.com>
2021-03-23 02:12:43 -0300

    Aula 188 - Gerando o html de fato (Writer)

--- app.js
+++ app.js
@@ -2,19 +2,22 @@ const Reader = require("./Reader");
 const Processor = require("./Processor");
 const Table = require("./Table");
 const HtmlParser = require("./HtmlParser");
+const Writer = require("./Writer");
 
 const reader = new Reader();
+
+const writer =  new Writer();
 
 async function main(){
     let data = await reader.Read("./user.csv");
 
    let dataProcessor = Processor.Process(data);
    let users = new Table(dataProcessor);
-   
-   users.rows.push(["Carlos", "Formação HTML", "PHP", "32"]);
 
    const html = await HtmlParser.Parse(users);
-   console.log(html);
+   
+
+    writer.Write(Date.now() + ".html", html);
 
 
 }
